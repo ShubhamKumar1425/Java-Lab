738870b19b09f204e18d816b509261194561323a
Shubham Kumar <rkshubham108@gmail.com>
2026-06-02 20:27:40 +0530

    Add functionality to reverse and display names

---
+++ Day-01/prog2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class prog2 {
+        public static void main(String[] args) {
+          
+        Scanner sc= new Scanner(System.in);
+          
+        System.out.println("ENTER THE FIRST NAME: ");
+        String f1=sc.nextLine();
+        System.out.println("ENTER THE SECOND NAME: ");
+        String s1=sc.nextLine();
+          
+        System.out.println("REVERSE ORDER IS:"+s1+" "+f1);
+        System.out.println("ENDS");
+    }        
+}
